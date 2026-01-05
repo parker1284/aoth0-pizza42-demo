1d8e0814e7ceb3ea09d24db0b3bfe4376aa378e8
Munoz.V <ricardo.munozv@hidglobal.com>
2026-01-05 14:23:14 -0600

    Rollback backend audience

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,7 @@ const checkJwt = jwt({
     jwksRequestsPerMinute: 5,
     jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
   }),
+  audience: process.env.AUTH0_API_AUDIENCE,
   issuer: `https://${process.env.AUTH0_DOMAIN}/`,
   algorithms: ["RS256"]
 });
